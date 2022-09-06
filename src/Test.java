5f2804ff28995436b503911af7b1d4e3f39a071e
Leon Moua <leonmoua@gmail.com>
2022-09-06 12:13:17 -0700

    first commit

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class helloworld {
+    public helloworld() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("\n Welcome to warmup Sept 6, 2022!");
+    }
+}
